892c6728786c3968635c3b85dba36157984c3efb
John B <100374955+jtbrennan@users.noreply.github.com>
2023-08-23 22:52:48 -0400

    Add files via upload

---
+++ myApp.js
@@ -0,0 +1,75 @@
+let express = require('express');
+let app = express();
+
+let bodyParser = require('body-parser');
+
+app.use("/public", express.static(__dirname + "/public"))
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next()
+})
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+})
+
+app.get("/json", (req, res) => {
+  if(process.env["MESSAGE_STYLE"] == "uppercase") {
+      res.json({"message": "HELLO JSON"});
+  }
+  else {
+      res.json({"message": "Hello json"});
+  }
+})
+
+app.get("/now", (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}, (req, res) => {
+  res.json({"time": req.time})
+})
+
+app.get("/:word/echo", (req, res) => {
+  res.json({echo: req.params.word})
+})
+
+app.get("/name", (req, res) => {
+  res.json({name: req.query.first + " " + req.query.last})
+})
+
+app.post("/name", (req, res) => {
+  res.json({name: req.body.first + " " + req.body.last})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
